ca04ca4b907eeae730c5f61343cb2ee35beeac88
svedbox <svedbox@gmail.com>
2020-08-04 22:03:41 +0300

    Adding base page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,8 @@
+
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+    
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
